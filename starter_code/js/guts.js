1058a486c4dda2e6bf4eb5a0618e5fd85b61392b
Nelly Ruiz <nelly.r.student@lyonscommunityschool.org>
2017-04-26 14:24:43 +0000

    nelyy

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -5,33 +5,41 @@
 
 function divide(x,y) {
 	//Write your code below this line
-
+var divide = x /y;
+return divide;
 }
 
 function multiply(x,y) {
-
+var multiply = x *y;
+return multiply;
 }
 
 function subtract(x,y) {
-
+var subtract = x -y;
+return subtract;
 }
 
 function add(x,y) {
-
+ var sum = x +y;
+ return sum;
 }
 
 function power(x,y) {
-
+var power = Math.pow (x,y);
+return power;
 }
 
 function square_root(x) {
-
+var square_root = Math.sqrt(x);
+return square_root;
 }
 
 function logBase10(x) {
-
+var logbase10 = Math.log10(x);
+return logbase10;
 }
 
 function generateRandomNumber() {
-
+var generateRandomNumber = Math.floor((Math.random()*10)+1);
+return generateRandomNumber;
 }
